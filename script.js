f9c04d8d644c55a4a9c3af14ef5d9f31c1cdb75e
Fox <zacnoa2.4@gmail.com>
2025-04-15 11:00:52 +0200

    image append

--- script.js
+++ script.js
@@ -16,15 +16,42 @@ function PosaljiEmail(meni)
         alert("nesto nije u redu");
     });
 }
+function CreateImage()
+{
+    let image=document.createElement("img");
+    image.src="style/assets/aaa.jpg";
+    document.getElementById("image-button").innerHTML="<3";
+    document.getElementById("mail-sent").appendChild(image);
+    image.style.width="300px";
+    image.style.height="auto";
+
+}
+
 
 
 
 document.addEventListener("DOMContentLoaded", function() {
-let meni1=document.getElementById("meni1");
-let meni2=document.getElementById("meni2");
-let image_button=document.getElementById("image-button");
-meni1.addEventListener("click", () => PosaljiEmail("Meni 1"));
-meni2.addEventListener("click", () => PosaljiEmail("Meni 2"));
+    let meni1 = document.getElementById("meni1");
+    let meni2 = document.getElementById("meni2");
+    let image_button = document.getElementById("image-button");
 
+    // Proverite da li su elementi pronađeni
+    if (meni1) {
+        meni1.addEventListener("click", () => PosaljiEmail("Meni 1"));
+    } else {
+        console.error("Element sa ID-jem 'meni1' nije pronađen.");
+    }
 
+    if (meni2) {
+        meni2.addEventListener("click", () => PosaljiEmail("Meni 2"));
+    } else {
+        console.error("Element sa ID-jem 'meni2' nije pronađen.");
+    }
+
+    if (image_button) {
+        image_button.addEventListener("click", () => CreateImage());
+    } else {
+        console.error("Element sa ID-jem 'image-button' nije pronađen.");
+    }
 });
+
